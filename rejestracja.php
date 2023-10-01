77d6d5c5c54f67f5a31f9c54d4c22441b73f372b
ZajacMateusz1 <144244766+ZajacMateusz1@users.noreply.github.com>
2023-10-01 02:11:05 +0200

    1

--- rejestracja.php
+++ rejestracja.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Rejestracja</title>
 </head>
 <body>
-    
+    <div class="glowny">
+    <input type="text" name="login" placeholder="login">
+    <input type="email" name="email" placeholder="email">
+    <input type="text" name="haslo" placeholder="Haslo">
+    <input type="text" name="haslo2" placeholder="Powtorz haslo">
+    <input type="submit" value="Zarejestruj się" name="wysli">    
+    </div>
 </body>
 </html>
