645461da36b15e2cd62d1b857b810eef1829bcfd
ZajacMateusz1 <144244766+ZajacMateusz1@users.noreply.github.com>
2023-10-02 19:17:01 +0200

    5

--- polbaza.php
+++ polbaza.php
@@ -3,8 +3,8 @@ $hostName = "localhost";
 $dbUser = "root";
 $dbPassword = "";
 $dbName = "baza_sklep";
-$conn = mysqli_connect ($hostName, $dbUser, $dbPassword, $dbName)
-if (!conn) {
-    die("Coś poszło nie tak")
+$conn = mysqli_connect ($hostName, $dbUser, $dbPassword, $dbName);
+if (!$conn) {
+    die("Coś poszło nie tak");
 }
 ?>
--- rejestracja.php
+++ rejestracja.php
@@ -25,8 +25,25 @@ if (isset($_POST["wys"])) {
     if (strlen($haslo) < 8) {
         array_push($bledy, "Hasło musi mieć co najmniej 8 znaków!");
     }
+    if (!preg_match("/[A-Z]/", $haslo) || !preg_match("/[0-9]/", $haslo)) {
+        array_push($bledy, "Hasło musi zawierać co najmniej jedną dużą literę i jedną cyfrę");
+    }
     if ($haslo !== $haslo2) {
         array_push($bledy, "Hasła nie są identyczne");
+    }
+    else {
+        require_once "polbaza.php";
+        $sql = "insert into users (login, email, haslo) values(?, ?, ?)";
+        $stmt = mysqli_stmt_init($conn);
+        $prepareStmt = mysqli_stmt_prepare($stmt,$sql);
+        if ($prepareStmt) {
+            mysqli_stmt_bind_param($stmt,"sss",$login,$email,$haslo);
+            mysqli_stmt_execute($stmt);
+            echo "<div class='alert alert-success'>Twoje konto zostało założone.</div>";
+        }
+        else{
+            die("Coś poszło nie tak");
+        }
     }
 }
 if (isset($_POST["wys"]) && count($bledy) > 0) {
